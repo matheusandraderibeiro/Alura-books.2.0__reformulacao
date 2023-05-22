f4bc4d599c64a7e930d1e1bb0e82bca1acf45e24
Matheus de Andrade Ribeiro <matheus.a.r.01020@gmail.com>
2023-05-22 15:43:03 -0300

    Update ordenar-por-preco_metodo-sort.js

--- src/js/ordenar-por-preco_metodo-sort.js
+++ src/js/ordenar-por-preco_metodo-sort.js
@@ -1,6 +1,6 @@
 const BTNprecoOrdenado = document.querySelectorAll (".preco");
 
-BTNprecoOrdenado.forEach (botao => {botao.addEventListener ("click", ordenarPreco)})
+BTNprecoOrdenado.forEach (botao => {botao.addEventListener ("click", ordenarPreco)});
 
 function ordenarPreco () {
     let livrosOrdenados = arrayDeLivros.sort ((a, b) => a.preco - b.preco);
